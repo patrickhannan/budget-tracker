fcfd3a7136fbabefa9307c5c54acfff385ae181c
Patrick Hannan <patrick@hannan.com>
2020-10-28 02:42:41 -0400

    Added process.env port

--- server.js
+++ server.js
@@ -15,14 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
