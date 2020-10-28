cb9984e5bd225cfb7776d16a988b0ad9563ab5b1
Patrick Hannan <patrick@hannan.com>
2020-10-28 02:33:01 -0400

    Added installation to service worker

---
+++ public/service-worker.js
@@ -0,0 +1,41 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "db.js",
+    "favicon.ico",
+    "index.js",
+    "manifest.webmanifest",
+    "service-worker.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "styles.css",
+  ];
+  
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+  
+// installation
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+      caches
+        .open(CACHE_NAME)
+        .then((cache) => {
+          console.log("Your files were pre-cached successfully!");
+          cache
+            .addAll(FILES_TO_CACHE)
+            .then((result) => {
+              console.log("result of add all", result);
+            })
+            .catch((err) => {
+              console.log("Add all error: ", err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    );
+  
+    self.skipWaiting();
+});
+  
+  
